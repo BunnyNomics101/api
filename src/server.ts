9ba0f599742c7f95fc305b4f0849730f4b57585d
dalideco <dalissj1@gmail.com>
2022-03-16 11:23:15 +0100

    implementing socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
 import express from 'express'
 import { Express } from "express";
 import chartData from './routes/chart'
+import { Server } from 'socket.io';
+import { createServer } from "http";
 
-const app:Express = express() ; 
-const PORT = process.env.NODE_ENV||3000;
+const app: Express = express();
+const PORT = process.env.NODE_ENV || 3000;
+const server = createServer(app)
+export const io = new Server(server)
 
 app.use(express.json())
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('hello',{message: "hello users"})
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
 app.use('/chart', chartData)
 
-app.listen(PORT , ()=>{
+server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
