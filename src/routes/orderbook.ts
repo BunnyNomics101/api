4484b2223dab58cb24ad2c157817861ba8b108ff
dalideco <dalissj1@gmail.com>
2022-03-17 16:25:08 +0100

    creating sending asks through asks route

--- src/routes/orderbook.ts
+++ src/routes/orderbook.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express'
+import { getAsks, getBids } from '../services/orderbook';
 const router = Router() ; 
 
-router.get('/asks/:address/:nbr', (req, res)=>{
-    res.status(200).send("asks")
-})
+router.get('/asks/:address/:nbr', getAsks)
 
-router.get('/bids/:address:/:nbr', (req, res)=>{
-    res.status(200).send("bids")
-})
+router.get('/bids/:address:/:nbr', getBids)
 
 export default router ; 
--- src/services/orderbook.ts
+++ src/services/orderbook.ts
@@ -1,10 +1,24 @@
 import SerumMarket from "../utils/SerumMarket";
 import { Request, Response } from "express";
+import { PublicKey } from "@solana/web3.js";
 
-export function getAsks(req: Request, res:Response){
-    
+export async function getAsks(req: Request, res:Response){
+    const marketAddress:string = req.params.address ; 
+    const nbr: number = parseInt(req.params.nbr) ; 
+
+    const serumMarket = new SerumMarket(
+        new PublicKey(marketAddress)
+    )
+
+    try{
+        await serumMarket.loadAll(); 
+        const data = serumMarket.getLNasks(nbr)
+        res.status(200).send(data)
+    }catch{
+        res.status(404).send('bad market')
+    }
 }
 
 export function getBids(req: Request, res:Response){
-    
+
 }
