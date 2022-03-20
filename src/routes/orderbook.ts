40b47979908a7ce5b0b3965246cb846188b1b6aa
dalideco <dalissj1@gmail.com>
2022-03-20 13:28:34 +0100

    adding function name in orderbook both route

--- src/routes/orderbook.ts
+++ src/routes/orderbook.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { getAsks, getBids } from '../services/orderbook';
+import { getBoth } from '../services/orderbook';
+
 const router = Router() ; 
 
 router.get('/asks/:address/:nbr', getAsks)
 
 router.get('/bids/:address/:nbr', getBids) 
 
-router.get('/:address/:nbr')
+router.get('/:address/:nbr', getBoth)
 
 
 export default router ; 
